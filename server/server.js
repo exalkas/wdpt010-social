159af98354c9023920e8f2d0a8ee55fb542ed742
Alkis Kastrisios <exalkas@yahoo.com>
2022-11-30 20:21:16 +0200

    updated server

--- server/server.js
+++ server/server.js
@@ -16,12 +16,6 @@ const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log('Server is up and running at port', port))
 
-const path = require('path');
-// console.log('PATH is', path.resolve(__dirname, 'client', 'build', 'index.html'))
-// console.log("🚀 ~ path.resolve(__dirname", path.resolve(__dirname))
-console.log("🚀 ~ __dirname", __dirname)
-
-
 // ADD THESE AT THE END OF SERVER
 app.use(express.static('client/build'));
 
